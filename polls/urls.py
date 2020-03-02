a36c35f9f79e50e85f1e5338fea0656d3df57fc8
nathaniellambert <60356150+nathaniellambert@users.noreply.github.com>
2020-03-01 21:33:30 -0500

    tutorial 3

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
 
 
@@ -13,7 +13,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
